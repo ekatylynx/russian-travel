0040fd8f0ffbf2f4372354108605db20b36193a8
Ekaterina V. <hitomizavr@gmail.com>
2025-01-26 17:14:41 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const getCurrentYear = () => new Date().getFullYear();
 
+// Автоматическое получение текущего года
 window.onload = () => {
   const yearNode = document.querySelector("#year");
   yearNode.textContent = getCurrentYear();
